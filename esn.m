828ccee97a75ef8ef3f3cca34465e42982c96b19
Hengjian Jia <henryjia18@yahoo.com>
2015-12-10 22:52:26 +0000

    Add feedback and looping over the same dataset.

--- esn.m
+++ esn.m
@@ -1,18 +1,21 @@
-function [final_internal_state, W_out, predicted] = esn(x, y, W_in, W, leaking_rate = 1)
+function [final_internal_state, W_out, predicted] = esn(x, y, W_in, W, W_b, leaking_rate = 1)
 
-  #lambda = 5;
   lambda = 0;
+  iters = 0;
   flush_length = 50;
   in_dim = size(x, 2) + 1;
   reservoir_size = size(W, 1);
+  noise = 0;
 
   internal_state = rescale(rand(reservoir_size, 1));
-  final_internal_state = zeros(size(x, 1) - flush_length, reservoir_size + in_dim);
-  update = zeros(reservoir_size, 1);  
+  final_internal_state = zeros(size(x, 1) * iters - flush_length, reservoir_size + in_dim);
+  update = zeros(reservoir_size, 1);
 
   # Run the reservoir
   for i = 1:size(x, 1)
-    update = tanh(W_in * [1; x(i, :)'] + W * internal_state);
+    rand("seed", 1e9 * now());
+    update = tanh(W_in * [1; x(i, :)'] + W * internal_state + W_b * y(i) / 10000 + rescale(rand(reservoir_size, 1), noise));
+    #update = tanh(W_in * [1; x(i, :)'] + W * internal_state + W_b * x(i, 6));
     internal_state = (1 - leaking_rate) * internal_state + leaking_rate * update;
 
     if (i > flush_length)
@@ -20,13 +23,24 @@ function [final_internal_state, W_out, predicted] = esn(x, y, W_in, W, leaking_r
     end
   end
 
-  targets = y(flush_length+1:end, 1);
+  for j = 1:iters
+  for i = 1:size(x, 1)
+    rand("seed", 1e9 * now());
+    update = tanh(W_in * [1; x(i, :)'] + W * internal_state + W_b * y(i) / 10000 + rescale(rand(reservoir_size, 1), noise));
+    #update = tanh(W_in * [1; x(i, :)'] + W * internal_state + W_b * x(i, 6));
+    internal_state = (1 - leaking_rate) * internal_state + leaking_rate * update;
+
+    final_internal_state(j * size(x, 1) + i - flush_length, :) = [1, x(i, :), internal_state'];
+  end
+  end
+
+  targets = [y(flush_length+1:end, 1); repmat(y, iters, 1)];
   data = final_internal_state;
 
-  W_out = pinv(data' * data + lambda * eye(in_dim + reservoir_size)) * data' * targets ;
+  W_out = pinv(data' * data + lambda * eye(in_dim + reservoir_size)) * data' * targets;
 
-  predicted = data * W_out;
+  predicted = (data * W_out)(iters * size(x, 1) + 1:end);
   figure(10)
-  plot(final_internal_state(end-50:end, randi([in_dim + 1, 100 + in_dim])))
+  plot(final_internal_state(end-250:end, randi([in_dim + 1, 100 + in_dim])))
   final_internal_state = final_internal_state(end, in_dim + 1:end);
 end
--- test.m
+++ test.m
@@ -1,15 +1,17 @@
-function test()
+function W_out = test()
 
-rand("seed", now());
+rand("seed", 1e9 * now());
+#rand("seed", 0);
 
 spectral_radius = 0.8;
 density = 0.95;
 reservoir_size = 100;
 in_dim = 12 + 1;
 W_in_radius = sqrt(6 / (reservoir_size + in_dim));
-#W_in_radius = 0.5;
+#W_in_radius = 0.1;
 #W_in_radius = 1 / sqrt(in_dim)
 [W_in, W] = gen_reservoir(reservoir_size, in_dim, W_in_radius, density, spectral_radius);
+W_b = rescale(rand(reservoir_size, 1));
 
 leaking_rate = 1;
 data = load("stock_data_clean/RDSA_LDX.TXT" )(1:end-200,:);
@@ -18,17 +20,21 @@ data_val = load("stock_data_clean/RDSA_LDX.TXT" )(end-199:end,:);
 [x_val, y_val] = gen_data(data_val);
 [x_norm, mu, sigma] = featureNormalize(x);
 x_val_norm = featureNormalizeKnown(x_val, mu, sigma);
-[final_internal_state, W_out,  predicted] = esn(x_norm, y, W_in, W, leaking_rate);
-[final_internal_state_val, predicted_val] = esn_generate(x_val_norm, final_internal_state, W_out, W_in, W, leaking_rate);
+
+[final_internal_state, W_out,  predicted] = esn(x_norm, y, W_in, W, W_b, leaking_rate);
+[final_internal_state_val, predicted_val] = esn_generate(x_val_norm, final_internal_state, W_out, W_in, W, W_b, leaking_rate);
 get_directional_accuracy(x(51:end, 6), predicted(1:end-1))
 get_directional_accuracy(x_val(:, 6), predicted_val(1:end-1))
 mean(abs(predicted(1:end - 1) ./ x(52:end, 6) - 1))
-mean(abs(predicted_val(1:end - 1) ./ x_val(2:end, 6) - 1))
+mean(abs(predicted_val(1 + 19:end - 1) ./ x_val(2 + 19:end, 6) - 1))
 figure(1)
 hold off
-plot(1:size(x_val, 1)-1, x_val(2:end, 6), "bo", "linewidth", 1)
+plot(1:size(x_val, 1)-1 - 19, x_val(2 + 19:end, 6), "b", "linewidth", 1)
 hold on
-plot(1:size(x_val, 1)-1, predicted_val(1:end - 1), "r+", "linewidth", 1)
+plot(1:size(x_val, 1)-1 - 19, predicted_val(1 + 19:end - 1), "r", "linewidth", 1)
 hold off
+
+mean(W_out(2:13))
+mean(W_out(14:end))
 
 end
